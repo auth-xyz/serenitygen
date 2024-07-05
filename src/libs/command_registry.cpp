4a902960cc539e02ddf0618408787153b0027c65
auth-xyz <authenticover@gmail.com>
2024-07-05 01:21:30 -0400

    ok now this is supposed to work.....

--- src/libs/command_registry.cpp
+++ src/libs/command_registry.cpp
@@ -1,15 +1,20 @@
 #include "../headers/command_registry.h"
 
 void CommandRegistry::register_command(const std::string& name, const CommandHandler& handler) {
-  commands[name] = handler;
+    auto it = commands.find(name);
+    if (it != commands.end()) {
+        // Command with the same name already exists
+        throw std::runtime_error("Command already registered: " + name);
+    }
+    commands[name] = handler;
 }
 
-void CommandRegistry::handle_command(const dpp::slashcommand_t& event) {
-  auto it = commands.find(event.command.get_command_name());
-  if (it != commands.end()) {
-    it->second(event);
-  } else {
-    event.reply("Unknown command!");
-  }
+void CommandRegistry::handle_command(const dpp::slashcommand_t& event) const {
+    auto it = commands.find(event.command.get_command_name());
+    if (it != commands.end()) {
+        it->second(event);
+    } else {
+        event.reply("Unknown command!");
+    }
 }
 
